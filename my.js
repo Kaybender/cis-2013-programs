59d13cf47905ed4e8c163969e5429f0ec5af9bc8
Kaybender <70542278+Kaybender@users.noreply.github.com>
2020-09-09 00:08:15 -0500

    Update my.js
    Added ")"

--- my.js
+++ my.js
@@ -7,7 +7,7 @@ var mainProgram = function ()
 {
     var stringFirstName = prompt("What is your first name?");
     if (!isNaN(stringFirstName)) {
-        alert("You did not enter a valid first name, so I will call you Toby.  Hello Toby!";
+        alert("You did not enter a valid first name, so I will call you Toby.  Hello Toby!");
     } else
     {
         alert("Hello "+ stringFirstName +"! It's great to meet you!");  
