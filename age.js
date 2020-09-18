7d111439f88a5f294bd9d9233d7d994621e50220
Kaybender <70542278+Kaybender@users.noreply.github.com>
2020-09-18 12:11:22 -0500

    Add files via upload
    Age HO#3

---
+++ age.js
@@ -0,0 +1,10 @@
+var floatAge,floatDay,intWeeks,intFortnights;
+     floatAge = parseFloat (prompt("Enter Current Age"));
+     floatDay = parseFloat(floatAge*365.25);
+     floatMonth = parseFloat(floatAge*12);
+     intWeeks = parseFloat(floatDays/7);
+     intFortnights = parseFloat(floatDay/14);
+     alert("Age in days = " + floatDay);
+     alert("Age in months = " + floatMonth);
+     alert("Age in weeks = " +  intWeeks);
+     alert("Age in fortnights =" + intFortnights);
